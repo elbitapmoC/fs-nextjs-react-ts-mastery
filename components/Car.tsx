f66e1a1a0b6fd6316306a80fd04346259d0e7112
Bazell Partridge <elbitapmoC@users.noreply.github.com>
2024-08-19 12:42:46 -0400

    Car parked, Car running, Time on road added.

--- components/Car.tsx
+++ components/Car.tsx
@@ -2,46 +2,51 @@
 
 import React, { useState, useEffect } from "react";
 import styles from "./Car.module.css";
-import { set } from "lodash";
 
 const Car = () => {
   const [color, setColor] = useState("red");
   const [timeOnRoad, setTimeOnRoad] = useState(0);
-  const [isMoving, setIsMoving] = useState(false);
-  const [isStopped, setIsStopped] = useState(true);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
+    if (!isRunning) return; // Exit early if the car is not running
+
     const timer = setInterval(() => {
-      setIsStopped(false);
-      setIsMoving(true);
       setTimeOnRoad((prevTime) => prevTime + 1);
     }, 1000);
 
-    return () => clearInterval(timer);
-  }, []);
+    return () => clearInterval(timer); // Cleanup on stop or unmount
+  }, [isRunning]);
+
+  const handleRunning = () => {
+    setIsRunning((prevRunning) => !prevRunning);
+  };
 
   return (
     <>
       <aside>
         <button
-          onClick={() => setColor("blue")}
+          onClick={() => setColor(color === "blue" ? "green" : "blue")}
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
-          Change to Blue
+          Change Dashboard Color
         </button>
         <button
-          onClick={() => setColor("green")}
+          onClick={handleRunning}
           type="button"
           className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         >
-          Change to Green
+          {isRunning ? "Stop" : "Start"}
         </button>
       </aside>
 
       <div className={`${styles.car} ${styles[color]}`}>Car</div>
-      {isMoving && <p>Have driven for {timeOnRoad} seconds</p>}
-      {isStopped && <p>Parked up atm..</p>}
+      <p>
+        {isRunning
+          ? `Have driven for ${timeOnRoad} seconds`
+          : "Parked up atm.."}
+      </p>
     </>
   );
 };
